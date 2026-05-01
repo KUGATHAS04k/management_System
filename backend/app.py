888aed4ce7e78056b2a5cc9a7f2f827f42f65538
kugathas <gkugathas04@gmail.com>
2026-05-01 11:34:35 +0100

    first day

---
+++ backend/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, jsonify
+from database import create_connection, create_table
+from flask_cors import CORS
+
+
+create_table()
+
+app = Flask(__name__)
+CORS(app)
+
+# Simple staff data (temporary)
+users = {
+    "S001": {
+        "pin": "1234",
+        "role": "staff"
+    },
+    "M001": {
+        "pin": "9999",
+        "role": "manager"
+    }
+}
+@app.route("/")
+def home():
+    return "RMS is running 🚀"
+
+# LOGIN API
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+
+    user_id = data.get("id")
+    pin = data.get("pin")
+
+    # check user exists
+    if user_id not in users:
+        return jsonify({"message": "User not found"}), 404
+
+    # check pin
+    if users[user_id]["pin"] != pin:
+        return jsonify({"message": "Wrong PIN"}), 400
+
+    return jsonify({
+        "message": "Login success",
+        "role": users[user_id]["role"]
+    })
+
+@app.route("/add-sale", methods=["POST"])
+def add_sale():
+    data = request.json
+
+    cash = data.get("cash", 0)
+    card = data.get("card", 0)
+    uber = data.get("uber", 0)
+    date = data.get("date")
+    staff_id = data.get("staff_id")
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO sales (cash, card, uber, date, staff_id)
+        VALUES (?, ?, ?, ?, ?)
+    """, (cash, card, uber, date, staff_id))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Sale saved"})
+
+@app.route("/sales",methods=["GET"])
+def get_sales():
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM sales")
+    rows = cursor.fetchall()
+
+    result = []
+    for row in rows:
+        result.append(dict(row))
+
+    conn.close()
+    return jsonify(result)
+# run server
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
---
+++ backend/database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def create_connection():
+    conn = sqlite3.connect('restaurant.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_table():
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS sales (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cash REAL,
+        card REAL,
+        uber REAL,
+        date TEXT,
+        staff_id TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
